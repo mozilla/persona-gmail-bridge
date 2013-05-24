36f257d22b5f260b9524a4668e7a9dd728cc98cf
Dan Callahan <dan.callahan@gmail.com>
2013-05-23 21:39:15 -0500

    Add i18n-abide

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-#!/usr/bin/env node
-
 const express = require('express');
+const i18n = require('i18n-abide');
 
 const app = express();
+
+app.use(i18n.abide({
+  supported_languages: ['en-US', 'it-CH'],
+  default_lang: 'en-US',
+  debug_lang: 'it-CH',
+  translation_directory: 'i18n'
+}));
 
 app.get('/.well-known/browserid', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
@@ -10,11 +16,11 @@ app.get('/.well-known/browserid', function (req, res) {
 });
 
 app.get('/provision', function (req, res) {
-  res.send('TODO: Add provisioning page');
+  res.send(req.gettext('TODO: Add provisioning page'));
 });
 
 app.get('/authenticate', function (req, res) {
-  res.send('TODO: Add authentication page');
+  res.send(req.gettext('TODO: Add authentication page'));
 });
 
 app.listen(3000);
