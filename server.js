06c9bb296ed96c5080e6ad67961c2c05af31ea9e
Dan Callahan <dan.callahan@gmail.com>
2013-05-23 23:08:55 -0500

    Add EJS templates, stub auth / provisioning pages

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 const i18n = require('i18n-abide');
 
 const app = express();
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
 
 app.use(i18n.abide({
   supported_languages: ['en-US', 'it-CH'],
@@ -20,11 +25,11 @@ app.get('/.well-known/browserid', function (req, res) {
 });
 
 app.get('/provision', function (req, res) {
-  res.send(req.gettext('TODO: Add provisioning page'));
+  res.render('provision');
 });
 
 app.get('/authenticate', function (req, res) {
-  res.send(req.gettext('TODO: Add authentication page'));
+  res.render('authenticate');
 });
 
 app.listen(3000);
