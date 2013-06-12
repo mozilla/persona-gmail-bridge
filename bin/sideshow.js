4a0920fb972ca5f275a40103309942ea902c8213
Sean McArthur <sean.monstar@gmail.com>
2013-06-12 09:50:47 -0700

    heartbeat checks google openid connection

--- bin/sideshow.js
+++ bin/sideshow.js
@@ -118,7 +118,12 @@ app.use(i18n.abide({
 // -- Express Routes --
 
 app.get('/__heartbeat__', function (req, res) {
-  res.send('ok');
+  openid.discover(googleEndpoint, true, function(err/*, providers*/) {
+    if (err) {
+      return res.status(500).end('bad');
+    }
+    res.send('ok');
+  });
 });
 
 app.get('/.well-known/browserid', function (req, res) {
