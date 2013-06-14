f6e8476cc8fef27db8661f258b03b8680d5a15e6
Sean McArthur <sean.monstar@gmail.com>
2013-06-13 17:23:00 -0700

    show button is disabled, use timeout for iframe

--- static/js/mismatch.js
+++ static/js/mismatch.js
@@ -7,19 +7,38 @@
 
   var tryAgain = document.getElementById('tryAgain');
 
+  function onFrameLoad(src, callback) {
+    var iframe = document.createElement('iframe');
+    iframe.style.visibility = 'hidden';
+    iframe.style.width = '1px';
+    iframe.style.height = '1px';
+    iframe.src = src;
+
+    var done = false;
+    function complete() {
+      if (done) {
+        return;
+      }
+      done = true;
+      callback();
+    }
+
+    iframe.onload = complete;
+    setTimeout(complete, 5000);
+
+    document.body.appendChild(iframe);
+  }
+
   if (tryAgain) {
+
     tryAgain.onclick = function() {
-      var iframe = document.createElement('iframe');
-      iframe.style.visibility = 'hidden';
-      iframe.style.width = '1px';
-      iframe.style.height = '1px';
-
-      iframe.src = 'https://accounts.google.com/Logout';
-      iframe.onload = function() {
+      if (tryAgain.disabled) {
+        return;
+      }
+      tryAgain.disabled = true;
+      onFrameLoad('https://accounts.google.com/Logout', function() {
         navigator.id.raiseAuthenticationFailure();
-      };
-
-      document.body.appendChild(iframe);
+      });
     };
   }
 
