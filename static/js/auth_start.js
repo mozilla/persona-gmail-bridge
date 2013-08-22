e190c064eba4663dff1f83d84d72d18f84719a13
Shane Tomlinson <stomlinson@mozilla.com>
2013-08-22 14:53:02 +0100

    Get the window resizing right for worst case scenario - user has multiple accounts.

--- static/js/auth_start.js
+++ static/js/auth_start.js
@@ -2,8 +2,25 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+var GOOGLE_AUTH_HEIGHT = 492;
+var GOOGLE_AUTH_WIDTH = 886;
+
+function resizeWindow() {
+  var viewportWidth = document.documentElement.clientWidth;
+  var resizeByX = GOOGLE_AUTH_WIDTH - viewportWidth;
+  // diff values stored in sessionStorage so the viewport can be resized back
+  // to its original size when the user returns from the IdP
+  sessionStorage.resizeByX = -resizeByX;
+
+  var viewportHeight = document.documentElement.clientHeight;
+  var resizeByY = GOOGLE_AUTH_HEIGHT - viewportHeight;
+  sessionStorage.resizeByY = -resizeByY;
+
+  window.resizeBy(resizeByX, resizeByY);
+}
+
 navigator.id.beginAuthentication(function (email) {
-  window.resizeBy(0, 50);
+  resizeWindow();
   var escapedEmail = encodeURIComponent(email);
   window.location = '/authenticate/forward?email=' + escapedEmail;
 });
