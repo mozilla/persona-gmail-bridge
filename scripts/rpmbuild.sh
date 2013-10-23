4715643dac9e30a281e95134fdeb069e673636be
Dan Callahan <dan.callahan@gmail.com>
2013-10-23 10:49:23 -0500

    Bump nodejs ver in RPM spec. No code changes.

--- scripts/rpmbuild.sh
+++ scripts/rpmbuild.sh
@@ -28,8 +28,8 @@ export GIT_HASH=$(git rev-parse --verify --quiet --short $VER);
 # export SVN_REVISION="$(svn info locale/ | sed -n -e "s,^Revision: ,,p")"
 export SVN_REVISION="118029"
 # export SIDESHOW_VER="$(echo $VER | sed 's/-/_/g').$GIT_HASH.$SVN_REVISION"
-export SIDESHOW_VER="0.9.6"
-export SIDESHOW_REL="20130926SHA${GIT_HASH}R${SVN_REVISION}"
+export SIDESHOW_VER="0.9.7"
+export SIDESHOW_REL="20131023SHA${GIT_HASH}R${SVN_REVISION}"
 
 if [ -z $GIT_HASH ]; then
     echo >&2 "Unrecognized git reference: $VER"
